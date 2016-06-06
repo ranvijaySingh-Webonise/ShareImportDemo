e1046efd08ef319a7cd2d1293770bf90dcaa8ccc
Rana Ranvijay Singh <rana.ranvijay@weboniselab.com>
2016-06-06 16:28:09 +0530

    Removed unused code

--- app/src/main/java/webonise/shareimportdemo/utils/GeoJsonParser.java
+++ app/src/main/java/webonise/shareimportdemo/utils/GeoJsonParser.java
@@ -4,8 +4,6 @@ import android.util.Log;
 
 import com.cocoahero.android.geojson.Feature;
 import com.cocoahero.android.geojson.FeatureCollection;
-import com.cocoahero.android.geojson.GeoJSON;
-import com.cocoahero.android.geojson.GeoJSONObject;
 import com.cocoahero.android.geojson.Polygon;
 import com.cocoahero.android.geojson.Ring;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -19,16 +17,6 @@ import webonise.shareimportdemo.models.SampleGeoJsonModel;
 
 public class GeoJsonParser {
     private static final String TAG = "GeoJsonParser";
-
-    public void tryThis() {
-        String geoJson = "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[73.77806425094604,18.518740020650107],[73.77619743347168,18.51102848922586],[73.77862215042114,18.510947099177976],[73.77995252609253,18.51391781084581],[73.78046751022339,18.51727505989433],[73.77806425094604,18.518740020650107]]]}}]}";
-        try {
-            GeoJSONObject geoJSONObject = GeoJSON.parse(geoJson);
-            geoJSONObject.getType();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
      * Function to convert Geojson model into geojson string
